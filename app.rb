2f7ed26a2495cb1f2051f55b60a549d22d9767b6
Jackwhitener <jackwhitener1@gmail.com>
2018-11-15 19:04:51 -0500

    Doubt this will actually work, but let's see

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ get '/' do
 	erb :page1
 end
 #THIS IS A COMMENT
+post '/list' do
+	list = params[:list]
+	list = list.split!
+	list = pair(options,2)
+	redirect 'page2?list=' + list
+end
+get '/page2' do
+	list = params[:list]
+	erb: page2 locals: {list: list}
+end
