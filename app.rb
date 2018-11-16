fa87dd53d3d62f0e5a26d6eeb6279cf936a7cf22
Jackwhitener <jackwhitener1@gmail.com>
2018-11-15 19:09:30 -0500

    yeah that whole thought process isn't working

--- app.rb
+++ app.rb
@@ -5,12 +5,8 @@ get '/' do
 end
 #THIS IS A COMMENT
 post '/list' do
-	list = params[:list]
-	list = list.split!
-	list = pair(options,2)
-	redirect 'page2?list=' + list
+
 end
 get '/page2' do
-	list = params[:list]
-	erb :page2, locals: {list: list}
+
 end
