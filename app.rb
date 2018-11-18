8f9bc5aa8f3fae0a972e933b0c0bcf94c5d859f6
Jackwhitener <jackwhitener1@gmail.com>
2018-11-17 19:15:44 -0500

    Adds js function for the text boxes thingy. Thanks steven. issue is, I don't know where the inputed strings go, or how to use them.

--- app.rb
+++ app.rb
@@ -3,10 +3,11 @@ require_relative 'random_pairs.rb'
 get '/' do
 	erb :page1
 end
-#THIS IS A COMMENT
 post '/list' do
-
+	list = params[:list]
+	redirect 'page2?list=' + list
 end
 get '/page2' do
-
+	list = params[:list]
+	erb :page2, locals: {list: list}
 end
